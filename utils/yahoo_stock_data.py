34c89e5742ef089e72626f2dea4fa9891de48fc7
unknown <1234@gmail.com>
2022-03-04 18:00:47 +0900

    kelly added

--- utils/yahoo_stock_data.py
+++ utils/yahoo_stock_data.py
@@ -12,6 +12,7 @@ class yahoo_data:
         self.start_date = kwargs.get('start','1900-01-01')
         self.end_date = kwargs.get('end','2200-01-01')
         self.ticker_list = kwargs.get('ticker_list',[''])
+        self.rebal_period = kwargs.get('rebal_period', '1D')
 
     def yahoo_stock_data_loading(self):
         return_df = pd.DataFrame()
@@ -20,6 +21,6 @@ class yahoo_data:
             adj_data = web.DataReader(ticker, 'yahoo', self.start_date, self.end_date)['Adj Close'].to_frame(ticker)
             return_df = pd.concat([return_df,adj_data], axis=1)
 
-        result_df = return_df.pct_change().dropna(axis=0)
+        result_df = return_df.resample(self.rebal_period).first().pct_change().dropna(axis=0)
         return result_df
 
