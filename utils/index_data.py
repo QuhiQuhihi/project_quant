ca07d5f8bb522391e911c3203fa5aa440946d7b1
QuhiQuhihi <quhiquhihi@gmail.com>
2022-03-21 18:25:14 +0900

    regime model in process

--- utils/index_data.py
+++ utils/index_data.py
@@ -14,7 +14,7 @@ class index_data:
         self.rebal_date = kwargs.get('rebal_date', '01')
 
     def index_data_loading(self):
-        base_dir = os.path.join('C:/','workspace','project_quant')
+        base_dir = os.path.join('/mnt','c','workspace','project_quant')
         index_data_dir = os.path.join(base_dir,'data','index_data')
 
         result_df = pd.DataFrame()
@@ -38,6 +38,47 @@ class index_data:
             input = pd.read_excel(os.path.join(index_data_dir, index_data_name_file[ind_name]), index_col='Date')
             input = input.sort_index(ascending=False)
             index_df = input.iloc[:, 0].resample(self.rebal_period).first().pct_change().fillna(0)
+            index_df.rename(ind_name, inplace=True)
+            index_df = index_df.loc[self.start_date: self.end_date]
+
+
+            result_df = pd.concat([
+                result_df,
+                index_df
+            ], axis=1).fillna(0)
+
+        result_df.index = pd.to_datetime(result_df.index)
+
+        return result_df
+    
+
+    def index_cum_data_loading(self):
+        base_dir = os.path.join('/mnt','c','workspace','project_quant')
+        index_data_dir = os.path.join(base_dir,'data','index_data')
+
+        result_df = pd.DataFrame()
+
+        index_data_name_file = {
+            'msci_acwi':'MSCI_All Country World.xlsx',
+            'msci_world':'MSCI World.xlsx',
+            'msci_emerging':'MSCI Emerging.xlsx',
+            'msci_world_gross':'MSCI World Gross.xlsx',
+            'msci_world_value':'MSCI World Value.xlsx',
+            'msci_real_estate':'MSCI World Real Estate.xlsx',
+            'bb_world_agg':'Bloomberg Barclays US AGG.xlsx',
+            'bb_emerging_agg':'Bloomberg Barclays EM AGG.xlsx',
+            'bb_corp_ig':'Bloomberg Barclays US Corp IG.xlsx',
+            'bb_corp_hy':'Bloomberg Barclays US Corp HY.xlsx',
+            'bb_infla_protect':'Bloomberg Barclays US Inflation Protected 7-10 year.xlsx',
+            'snp_commodity':'SNP GSCI TR.xlsx',
+        }
+        for ind_name in index_data_name_file.keys():
+            # load_index_data(self.start_date, self.end_date, index_data_dir,index_data_name_file[ind_name],ind_name)
+            input = pd.read_excel(os.path.join(index_data_dir, index_data_name_file[ind_name]), index_col='Date')
+            input = input.sort_index(ascending=False)
+            reb_index = input.iloc[:, 0].resample(self.rebal_period).first()
+            index_df = reb_index / reb_index.iloc[0]
+            index_df = index_df.fillna(1)
             index_df.rename(ind_name, inplace=True)
             index_df = index_df.loc[self.start_date: self.end_date]
 
